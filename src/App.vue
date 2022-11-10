0e1016e3456ff7751c190cbf5ebe8fc6fd54b28a
0xjoshva <joshuasteed123@gmail.com>
2022-11-10 16:22:52 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,47 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+
+*, *::before, *::after {
+  box-sizing: border-box;
+}
+* {
+  margin: 0;
+}
+html, body {
+  height: 100%;
+}
+body {
+  line-height: 1.5;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
+img, picture, video, canvas, svg {
+  display: block;
+  max-width: 100%;
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+input, button, textarea, select {
+  font: inherit;
 }
+p, h1, h2, h3, h4, h5, h6 {
+  overflow-wrap: break-word;
+}
+#root, #__next {
+  isolation: isolate;
+}
+:root {
+  /*Primary */
+  --Moderateblue: hsl(238, 40%, 52%);
+  --SoftRed: hsl(358, 79%, 66%);
+  --Lightgrayishblue: hsl(239, 57%, 85%);
+  --Palered: hsl(357, 100%, 86%);
 
-nav a.router-link-exact-active {
-  color: #42b983;
+  /* Neutral */
+  --Darkblue: hsl(212, 24%, 26%);
+  --GrayishBlue: hsl(211, 10%, 45%);
+  --Lightgray: hsl(223, 19%, 93%);
+  --Verylightgray: hsl(228, 33%, 97%);
+  --White: hsl(0, 0%, 100%);
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,95 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+    <section>
+      <div class="message" v-for="comment in messages.comments" :key="comment.id">
+      <div class="comment" v-if="messages.comments != null">
+        <div class="points">
+        <button><img src="../assets/icon-plus.svg" alt=""></button>
+        <div class="count">1</div>
+        <button><img src="../assets/icon-minus.svg" alt=""></button>
+        </div>
+      </div>
+
+      <div class="reply" v-for="reply in comment.replies" :key="reply.id">
+        {{ reply.id }}
+      </div>
+    </div>
+    </section>
+  </main>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import messageData from "/data/data.json";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  data() {
+    return {
+      messages: messageData,
+    };
+  },
+};
 </script>
+<style scoped>
+
+main {
+  background-color: hsl(228, 33%, 97%);
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+section {
+  width: 45rem;
+  height: 90vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 1.5rem;
+}
+.comment{
+background: white;
+  width: 100%;
+  height: 10rem;
+  border-radius: 8px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.055);
+}
+.reply{
+  background: white;
+  height: 10rem;
+  width: 85%;
+  display: flex;
+  align-self: flex-end;
+  border-radius: 8px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.055);
+}
+.message{
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  height:fit-content;
+  row-gap: 1.5rem;
+}
+.points{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: fit-content;
+  padding: .5rem;
+  padding-block: 1rem;
+
+  height: fit-content;
+  row-gap: 1rem;
+  border-radius: 4px;
+  background-color:var(--Verylightgray);
+}
+.points button{
+  border: none;
+  background: none;
+
+}
+</style>
