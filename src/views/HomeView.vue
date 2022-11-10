208d860d1e188b21b47ffd8895e7769c6b0d36ca
0xjoshva <joshuasteed123@gmail.com>
2022-11-10 22:29:23 +0200

    formatting

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,36 @@
 <template>
   <main>
     <section>
-      <div class="message" v-for="comment in messages.comments" :key="comment.id">
-      <div class="comment" v-if="messages.comments != null">
-        <div class="points">
-        <button @click="comment.score++"><img src="../assets/icon-plus.svg" alt=""></button>
-        <div class="count">{{comment.score}}</div>
-        <button @click="comment.score--"><img src="../assets/icon-minus.svg" alt=""></button>
-        </div>
-        <div class="right">
-          <div class="details" v-for="user in comment" :key="user.username">
-            <img :src="comment.user.image.png" alt="">
-          <p class="username">{{user.username}}</p>
+      <div
+        class="message"
+        v-for="comment in messages.comments"
+        :key="comment.id"
+      >
+        <div class="comment" v-if="messages.comments != null">
+          <div class="points">
+            <button @click="comment.score++">
+              <img src="../assets/icon-plus.svg" alt="" />
+            </button>
+            <div class="count">{{ comment.score }}</div>
+            <button @click="comment.score--">
+              <img src="../assets/icon-minus.svg" alt="" />
+            </button>
           </div>
-          <div class="msgcontent">
-            <p>
-              {{}}
-            </p>
+          <div class="right">
+            <div class="details" v-for="user in comment" :key="user.username">
+              <img :src="comment.user.image.png" alt="" />
+              <p class="username">{{ user.username }}</p>
+            </div>
+            <div class="msgcontent">
+              <p>{{}}</p>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="reply" v-for="reply in comment.replies" :key="reply.id">
-        {{ reply.id }}
+        <div class="reply" v-for="reply in comment.replies" :key="reply.id">
+          {{ reply.id }}
+        </div>
       </div>
-    </div>
     </section>
   </main>
 </template>
@@ -43,7 +49,6 @@ export default {
 };
 </script>
 <style scoped>
-
 main {
   background-color: hsl(228, 33%, 97%);
   width: 100%;
@@ -60,16 +65,16 @@ section {
   align-items: center;
   row-gap: 1.5rem;
 }
-.comment{
-background: white;
+.comment {
+  background: white;
   width: 100%;
   height: 10rem;
   border-radius: 8px;
   box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.055);
   display: flex;
-    align-items: center;
+  align-items: center;
 }
-.reply{
+.reply {
   background: white;
   height: 10rem;
   width: 85%;
@@ -78,38 +83,35 @@ background: white;
   border-radius: 8px;
   box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.055);
 }
-.message{
+.message {
   display: flex;
   flex-direction: column;
 
   width: 100%;
-  height:fit-content;
+  height: fit-content;
   row-gap: 1.5rem;
 
   align-items: center;
 }
-.points{
+.points {
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   width: fit-content;
-  padding: .5rem;
+  padding: 0.5rem;
   padding-block: 1rem;
   margin-left: 1.8rem;
 
   height: fit-content;
   row-gap: 1rem;
   border-radius: 4px;
-  background-color:var(--Verylightgray);
+  background-color: var(--Verylightgray);
   color: var(--Moderateblue);
   font-weight: bold;
-
 }
-.points button{
+.points button {
   border: none;
   background: none;
-
-
 }
 </style>
